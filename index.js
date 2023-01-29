4ae744ece8f6038d44a14f64cc2ad90bcf65acc7
masud90895 <masudhossainmbs129@gmail.com>
2023-01-29 18:26:05 +0600

    bills generating id added

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const { MongoClient,ObjectId, ServerApiVersion } = require("mongodb");
+const { MongoClient, ObjectId, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 //gitignore
-require('dotenv').config();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // used Middleware
@@ -14,11 +14,10 @@ app.use(cors());
 app.use(express.json());
 
 //jwt token
-const secretKey = process.env.SECRET_KEY 
-
+const secretKey = process.env.SECRET_KEY;
 
 // Connect With MongoDb Database
-const uri =process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -28,110 +27,114 @@ const client = new MongoClient(uri, {
 // Create a async function to all others activity
 async function run() {
   try {
-   
-
     // Create Database to store Data
     const userCollection = client.db("programmingHeroTask").collection("users");
     const billCollection = client.db("programmingHeroTask").collection("bills");
 
-    // user registration 
+    // user registration
     app.post("/api/registration", async (req, res) => {
-        const { firstName, lastName, email, password } = req.body;
-        
-        const encryptedPassword = await bcrypt.hash(password, 10);
-        console.log(encryptedPassword);
-        try {
-          const oldUser = await userCollection.findOne({ email : email });
-      
-          if (oldUser) {
-            return res.json({ error: "User Exists" });
-          }
-          await userCollection.insertOne({
-            firstName,
-            lastName,
-            email,
-            password: encryptedPassword
-          });
-          res.send({ status: "ok" });
-        } catch (error) {
-          res.send({ status: "error" });
+      const { firstName, lastName, email, password } = req.body;
+
+      const encryptedPassword = await bcrypt.hash(password, 10);
+      console.log(encryptedPassword);
+      try {
+        const oldUser = await userCollection.findOne({ email: email });
+
+        if (oldUser) {
+          return res.json({ error: "User Exists" });
         }
-      });
+        await userCollection.insertOne({
+          firstName,
+          lastName,
+          email,
+          password: encryptedPassword,
+        });
+        res.send({ status: "ok" });
+      } catch (error) {
+        res.send({ status: "error" });
+      }
+    });
 
+    // user login
+    app.post("/api/login", async (req, res) => {
+      const { email, password } = req.body;
 
-      // user login 
-      app.post("/api/login", async (req, res) => {
-        const { email, password } = req.body;
-      
-        const user = await userCollection.findOne({ email });
-        if (!user) {
-          return res.json({ error: "User Not found" });
+      const user = await userCollection.findOne({ email });
+      if (!user) {
+        return res.json({ error: "User Not found" });
+      }
+      if (await bcrypt.compare(password, user.password)) {
+        const token = jwt.sign({ email: user.email }, secretKey, {
+          expiresIn: "7d",
+        });
+
+        if (res.status(201)) {
+          return res.json({ status: "ok", data: token });
+        } else {
+          return res.json({ error: "error" });
         }
-        if (await bcrypt.compare(password, user.password)) {
-          const token = jwt.sign({ email: user.email }, secretKey, {
-            expiresIn: "7d",
-          });
-      
-          if (res.status(201)) {
-            return res.json({ status: "ok", data: token });
-          } else {
-            return res.json({ error: "error" });
+      }
+      res.json({ status: "error", error: "InvAlid Password" });
+    });
+
+    // get user data
+
+    app.post("/api/getUserData", async (req, res) => {
+      const { token } = req.body;
+      try {
+        const user = jwt.verify(token, secretKey, (err, res) => {
+          if (err) {
+            return "token expired";
           }
+          return res;
+        });
+        console.log(user);
+        if (user == "token expired") {
+          return res.send({ status: "error", data: "token expired" });
         }
-        res.json({ status: "error", error: "InvAlid Password" });
-      });
 
-
-      // get user data 
-
-
-      app.post("/api/getUserData", async (req, res) => {
-        const { token } = req.body;
-        try {
-          const user = jwt.verify(token, secretKey, (err, res) => {
-            if (err) {
-              return "token expired";
-            }
-            return res;
-          });
-          console.log(user);
-          if (user == "token expired") {
-            return res.send({ status: "error", data: "token expired" });
-          }
-      
-          const useremail = user.email;
-          userCollection.findOne({ email: useremail })
-            .then((data) => {
-              res.send({ status: "ok", data: data });
-            })
-            .catch((error) => {
-              res.send({ status: "error", data: error });
-            });
-        } catch (error) { }
-      });
-
-      // post bills 
-
-      app.post("/api/add-billing",async (req, res) => {
-        const result = await billCollection.insertOne(req.body)
-        if(result.insertedId){
-          res.send({
-            success: true,
-            message : `Success Created ${req.body.fullName}`
+        const useremail = user.email;
+        userCollection
+          .findOne({ email: useremail })
+          .then((data) => {
+            res.send({ status: "ok", data: data });
           })
-        }
-        else{
-          res.send({success: false, message: "Can't insert bill"})
-        }
-      })
+          .catch((error) => {
+            res.send({ status: "error", data: error });
+          });
+      } catch (error) {}
+    });
 
+    // post bills
 
+    app.post("/api/add-billing", async (req, res) => {
+      const generatingId = new ObjectId().toString();
 
+      const { fullName, email, phone, amount, time, AddedUserEmail } = req.body;
+      const addedData = {
+        generatingId,
+        fullName,
+        email,
+        phone,
+        amount,
+        time,
+        AddedUserEmail,
+      };
+      const result = await billCollection.insertOne(addedData)
+      if(result.insertedId){
+        res.send({
+          success: true,
+          message : `Success Created ${req.body.fullName}`
+        })
+      }
+      else{
+        res.send({success: false, message: "Can't insert bill"})
+      }
+    });
   } finally {
     // await client.close();
   }
 }
-
 
 // Call the function you decleare abobe
 run().catch(console.dir);
@@ -140,6 +143,5 @@ run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Hello From server!");
 });
-
 
 app.listen(port, () => console.log(`Server up and running ${port}`));
